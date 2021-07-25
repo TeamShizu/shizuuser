7309ef48534522a92b2cb75d0a66471993797d64
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-07-25 18:06:25 +0530

    Create __init__.py

---
+++ assistant/manager/__init__.py
@@ -0,0 +1,11 @@
+# Shizu - UserBot
+# Copyright (C) 2021 TeamShizu
+#
+# This file is a part of < https://github.com/TeamShizu/ShizuUser/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamShizu/ShizuUser/blob/main/LICENSE/>.
+
+
+from plugins import *
+
+asst = asst
