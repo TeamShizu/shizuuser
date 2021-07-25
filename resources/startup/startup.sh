4cce0f9367e7fa26908c6a4960a4bcd9509e0067
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-07-25 20:50:54 +0800

    Add files via upload

--- resources/startup/startup.sh
+++ resources/startup/startup.sh
@@ -1,1 +1,19 @@
+#!/usr/bin/env bash
+# Shizu - UserBot
+# Copyright (C) 2021 TeamShizu
+#
+# This file is a part of < https://github.com/TeamShizu/ShizuUser/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/TeamShizu/ShizuUser/blob/main/LICENSE/>.
 
+
+echo "
+            ┏┳┓╋┏┓╋╋╋╋┏┓┏┓
+            ┃┃┣┓┃┗┳┳┳━╋╋┛┃
+            ┃┃┃┗┫┏┫┏┫╋┃┃╋┃
+            ┗━┻━┻━┻┛┗━┻┻━┛
+
+      Visit @ShizuUpdates for updates!!
+
+"
+
+python3 -m pyShizuUser
