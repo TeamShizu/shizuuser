dbaf6925abe4e5fdedaed08fba78e2449af4f69d
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-07-25 18:07:41 +0530

    Create __init__.py

---
+++ assistant/pmbot/__init__.py
@@ -0,0 +1,7 @@
+from pyShizuUser.functions.asst_fns import *
+from pyShizuUser.functions.botchat_db import *
+
+from .. import *
+
+OWNER_NAME = ShizuUser_bot.me.first_name
+OWNER_ID = ShizuUser_bot.me.id
