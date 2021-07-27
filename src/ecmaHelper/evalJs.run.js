8590ed15ae050d74079bdb29558c6d42073d0d75
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-07-27 16:16:22 +0530

    Add files via upload

--- src/ecmaHelper/evalJs.run.js
+++ src/ecmaHelper/evalJs.run.js
@@ -1,9 +1,9 @@
-// Shizu - UserBot
+// shizuuser - UserBot
 // Copyright (C) 2020 TeamShizu
 //
-// This file is a part of < https://github.com/TeamShizu/ShizuUser/ >
+// This file is a part of < https://github.com/TeamShizu/shizuuser/ >
 // PLease read the GNU Affero General Public License in
-// <https://www.github.com/TeamShizu/ShizuUser/blob/main/LICENSE/>.
+// <https://www.github.com/TeamShizu/shizuuser/blob/main/LICENSE/>.
 
 'use-strict';
 sgrywryhio
